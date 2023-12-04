216138b441d447085a76a5eddb666e0471b8ae49
rhopper2021 <rhopper2021@hightechhigh.org>
2023-12-03 18:11:53 -0800

    Start of the weather route code branch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -15,13 +15,20 @@ app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
 
 my_data = {}
-endpoint = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=FeJBzzela4w41klzeTQ0rYP056JXSWJX"
+endpoint = 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=vLbGMAWNX6B0a0sRd9Wg16E6HgbNtEJ7'
+# "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=FeJBzzela4w41klzeTQ0rYP056JXSWJX"
 try:
     r = requests.get(endpoint)
     data = r.json()
-    pprint(data)
+    #pprint(data)
 except:
     print('please try again')
+
+# url = "https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=XXX"
+# headers = {"accept": "application/json"}
+# response = requests.get(url, headers=headers)
+
+# print(response.text)
 
 class Location(FlaskForm):
     city = StringField(
@@ -36,25 +43,17 @@ class Location(FlaskForm):
         'Zip Code', 
         validators=[DataRequired()]
     )
-# class Playlist(FlaskForm):
-#     song_title = StringField(
-#         'Song Title', 
-#         validators=[DataRequired()]
-#     )
 
-
-# route decorator binds a function to a URL
-# @app.route('/', methods=('GET', 'POST'))
-# def hello():
-#     form = Playlist()
-#     if form.validate_on_submit():
-#         return redirect('/view_playlist')
-#     return render_template('index.html', form=form)
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
     form = Location()
     if form.validate_on_submit():
         # store_location(form.song_title.data, form.song_artist.data)
-        return redirect('/view_playlist')
+        return redirect('/weather')
     return render_template('temp.html', form=form)
+
+@app.route('/weather')
+def display():
+    return render_template('weathertemp.html')
+#Want to get fire, lightning, precipitation, wind speed info
